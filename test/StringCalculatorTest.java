3337851d16e66a6581e8764a612e27a17ed6a129
bklopcic <benjaminklopcic@gmail.com>
2018-01-30 18:57:57 -0500

    test for adding two numbers

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -17,14 +17,27 @@ class StringCalculatorTest {
     }
 
     @Test
-    void addTest()
+    void singleDigitAddTest()
     {
         StringCalculator calculator = new StringCalculator();
 
-        //single number
         String[] testVals = {"-9999", "-1", "0", "000", "1", "7", "2147483647"};
         for (int  i = 0; i < testVals.length; i++) {
             assertEquals(Integer.parseInt(testVals[i]), calculator.add(testVals[i]), "Failed to add single number");
         }
+    }
+
+    @Test
+    void twoNumberAddTest()
+    {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(-19998, calculator.add("-9999,-9999"), "Failed to add two numbers");
+        assertEquals(0, calculator.add("1,-1"), "Failed to add two numbers");
+        assertEquals(-2, calculator.add("-1,-1"), "Failed to add two numbers");
+        assertEquals(2, calculator.add("1,1"), "Failed to add two numbers");
+        assertEquals(0, calculator.add("0,0"), "Failed to add two numbers");
+        assertEquals(23, calculator.add("7,16"), "Failed to add two numbers");
+        assertEquals(64197, calculator.add("9876,54321"), "Failed to add two numbers");
     }
 }
