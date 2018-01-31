3cd1169f7c6db138ea2e36f80dbd9ffb7312db26
bklopcic <benjaminklopcic@gmail.com>
2018-01-30 19:40:48 -0500

    teast for adding n numbers

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -34,10 +34,23 @@ class StringCalculatorTest {
 
         assertEquals(-19998, calculator.add("-9999,-9999"), "Failed to add two numbers");
         assertEquals(0, calculator.add("1,-1"), "Failed to add two numbers");
+        assertEquals(0, calculator.add("-1,1"), "Failed to add two numbers");
         assertEquals(-2, calculator.add("-1,-1"), "Failed to add two numbers");
         assertEquals(2, calculator.add("1,1"), "Failed to add two numbers");
         assertEquals(0, calculator.add("0,0"), "Failed to add two numbers");
         assertEquals(23, calculator.add("7,16"), "Failed to add two numbers");
         assertEquals(64197, calculator.add("9876,54321"), "Failed to add two numbers");
+    }
+
+    @Test
+    void nNumberAddTest()
+    {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(-29997, calculator.add("-9999,-9999,-9999"), "Failed to add three numbers");
+        assertEquals(9806, calculator.add("-1,23,0,9782"), "Failed to add five numbers");
+        assertEquals(0, calculator.add("0,0,0,0,0"), "Failed to add five zeros");
+        assertEquals(99, calculator.add("2,5,-10,54,7,19,22"), "Failed to add 7 numbers");
+        assertEquals(36, calculator.add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"), "Failed to add 36 numbers");
     }
 }
